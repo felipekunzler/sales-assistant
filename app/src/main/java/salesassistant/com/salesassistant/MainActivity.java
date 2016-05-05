a0fccc95b84e146f37d4aca88076d1d74f47384b
Felipe Kunzler <felipekunzler@gmail.com>
2016-05-05 10:58:41 -0300

    Add empty android project

---
+++ app/src/main/java/salesassistant/com/salesassistant/MainActivity.java
@@ -0,0 +1,13 @@
+package salesassistant.com.salesassistant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
